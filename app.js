10061448d31adfc8b1def3fac81b3a314e0492b9
lakshya-yadav9 <lakshyay.bba2023@ssism.org>
2025-11-19 14:37:34 +0530

    Update port configuration for deployment

--- app.js
+++ app.js
@@ -112,6 +112,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error.ejs", { message });
 });
 
-app.listen(8080, () => {
-  console.log(`Server is running on port 8080`);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
